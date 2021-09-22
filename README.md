ba1e4ccedf843bbadcdbda38c054e2b2ce3846a3
Limosity <illimosity@gmail.com>
2021-09-22 17:31:25 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# NJU-Network-Authenticate
+
+登录南京大学校园网的脚本，支持验证码识别。
+
+登录部分参考：https://github.com/forewing/nju-health-checkin
+
+验证码部分（muggle-ocr）使用：https://github.com/kerlomz/captcha_trainer
+
+## 用法
+
+首先 clone 代码，安装 python3 环境，并安装必要依赖：
+
+```shell
+pip install -r requirements.txt
+```
+
+如果不需要验证码识别功能，则不需要保存 `muggle_ocr` 目录，也不需要安装多余的依赖，只需 `pip install -r requirements-no-captcha.txt`。
+
+安装完成之后，执行
+
+```shell
+python3 login.py [username] [password]
+```
+
+并将 `username` 和 `password` 替换为统一认证的用户名和密码即可。
+
+也可设置环境变量 `NJU_USERNAME` 和 `NJU_PASSWORD` 后直接执行 `python3 login.py`。
+
+## 登出
+
+```shell
+curl "http://p.nju.edu.cn/api/portal/v1/logout" --data '{"domain":"default"}'
+```
+
---
+++ login.py
@@ -0,0 +1,82 @@
+import requests
+from bs4 import BeautifulSoup
+import json, datetime
+import sys, os
+
+import random
+import base64
+from Crypto.Cipher import AES
+from Crypto.Util import Padding
+
+if len(sys.argv) == 3:
+  username = sys.argv[1]
+  password = sys.argv[2]
+else:
+  username = os.environ['NJU_USERNAME']
+  password = os.environ['NJU_PASSWORD']
+
+url = "https://authserver.nju.edu.cn/authserver/login?service=http%3A%2F%2Fp.nju.edu.cn%2Fcas%2F"
+
+# Reference: https://github.com/forewing/nju-health-checkin/blob/master/checkin.py
+def encryptAES(data, key):
+  def rds(len):
+    return ''.join(random.choices("ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678", k=len))
+  encrypt = AES.new(key.strip().encode('utf-8'), AES.MODE_CBC, rds(16).encode('utf-8'))
+  return base64.b64encode(encrypt.encrypt(Padding.pad((rds(64) + data).encode('utf-8'), 16))).decode('utf-8')
+
+def getInput(soup, id=None, name=None):
+  if id:
+    return soup.find("input", {"id": id})['value']
+  elif name:
+    return soup.find("input", {"name": name})['value']
+
+s = requests.Session()
+r = s.get(url)
+try:
+  soup = BeautifulSoup(r.text, 'html.parser')
+  data = {
+    'username': username,
+    'password': encryptAES(password, getInput(soup, id="pwdDefaultEncryptSalt")),
+    'lt': getInput(soup, name="lt"),
+    'dllt': "userNamePasswordLogin",
+    'execution': getInput(soup, name="execution"),
+    '_eventId': getInput(soup, name="_eventId"),
+    'rmShown': getInput(soup, name="rmShown"),
+  }
+except:
+  print("Failed to retrieve login information")
+
+r = requests.get("https://authserver.nju.edu.cn/authserver/needCaptcha.html?username=" + username)
+if r.text == "true":
+  print("CAPTCHA required")
+  
+  print("Initializing OCR...")
+  import muggle_ocr
+  os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+  
+  print("Obtaining CAPTCHA...")
+  res = s.get("https://authserver.nju.edu.cn/authserver/captcha.html")
+  
+  print("Solving CAPTCHA...")
+  sdk = muggle_ocr.SDK(model_type = muggle_ocr.ModelType.Captcha)
+  captcha_text = sdk.predict(image_bytes = res.content)
+  print("CAPTCHA:", captcha_text)
+  
+  data["captchaResponse"] = captcha_text
+
+print("Logging in...")
+s.post(url, data)
+
+r = requests.get("http://p.nju.edu.cn/api/selfservice/v1/volume/" + datetime.datetime.now().strftime("%m"))
+try:
+  data = json.loads(r.text)
+except:
+  print(r.text)
+  print("Failed to parse data")
+
+if data["reply_code"] == 403:
+  print(r.text)
+  print("Failed to login")
+else:
+  total_secs = data["results"]["rows"][0]["total_time"]
+  print("Uptime: {}h{}m{}s".format(total_secs // 3600, total_secs % 3600 // 60, total_secs % 60))
